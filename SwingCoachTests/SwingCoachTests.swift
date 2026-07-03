9fad56de2cbac5fe5ba7b74097d56a49e01176a1
GEORGE FASHHO <georgefashho2003@gmail.com>
2026-07-03 13:30:02 -0500

    Initial Commit

---
+++ SwingCoachTests/SwingCoachTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwingCoachTests.swift
+//  SwingCoachTests
+//
+//  Created by George Fashho on 7/3/26.
+//
+
+import Testing
+@testable import SwingCoach
+
+struct SwingCoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
